1b3184c47268900d98b5ba990ca59b8901165768
sanryuu <cpu.cpu.cpu9@gmail.com>
2013-10-20 19:31:33 +0900

    リファレンスを取得する対象のサンプルファイルを作成した。
    コメントの開始記号と終了記号のあるものの方が処理しやすいものにする。
    最初は、PHPを使う。

---
+++ sample/sample.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * test入力1
+ *
+ * test_funcを出力する
+ *
+ */
+function test_func1(){
+  print "test_func1"
+}
+
+/**
+ * test入力2
+ *
+ */
+function test_func2(){
+  print "test_func2"
+}
+
+
+?>
